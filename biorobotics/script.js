cd712fe2eedc5464bddd79e289b673c88687df43
michellewoohoo <woomy@uci.edu>
2017-09-13 11:02:58 -0700

    changed theme and updated publications

--- biorobotics/script.js
+++ biorobotics/script.js
@@ -31,3 +31,13 @@ function changeImage(){
 }
 
 changeImage();
+
+function show(id){
+    var x=document.getElementById(id+'b');
+    if(x.style.display==='none'){
+        x.style.display='block';
+    }else{
+        x.style.display='none';
+    }
+  
+}
