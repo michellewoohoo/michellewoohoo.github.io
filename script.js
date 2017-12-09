53ae59324e908e89778cad1ee3edc51d0452a5b0
michellewoohoo <michelleymwoo@gmail.com>
2017-12-08 21:28:29 -0800

    added changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-var imageArray=["slideshow/biorobotics.png", "slideshow/gradetitle.png","slideshow/textedit.jpg", 
+var imageArray=["slideshow/medapp.png","slideshow/biosite.png","slideshow/genes.png", "slideshow/gradetitle.png","slideshow/textedit.jpg", 
 "slideshow/maze.jpg", "slideshow/othello.png","slideshow/spendtrack.jpg","slideshow/catmario.jpg"];
 
 
-var descrip=["UCI Biorobotics Engineering Website", "Class Grade/Final Score Calculator Website",
+var descrip=["AR MedKit App","UCI Biorobotics Engineering Website","FitYourGenes", "Class Grade/Final Score Calculator Website",
 "Text Editor", "Maze Generator/Solver", "Othello + AI", "Spending Tracker", "Cat Mario Game"];
 
 
